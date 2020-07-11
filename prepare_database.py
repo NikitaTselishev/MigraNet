bcc8c5a72c8e630552403413691f18fc613297d3
Nikita Tselishev <48827175+NikitaTselishev@users.noreply.github.com>
2020-07-11 20:07:27 +0300

    Update prepare_database.py

--- prepare_database.py
+++ prepare_database.py
@@ -77,14 +77,36 @@ def main():
                            "chat_name TEXT NOT NULL, "
                            "FOREIGN KEY (owner) REFERENCES users (user_id) ON DELETE CASCADE"
                            ");")
-
+            
+            #insert into chat_ids
+            cursor.execute(sql.SQL("insert into chat_ids values (2, 1, 'Футболисты' );"))
+            cursor.execute(sql.SQL("insert into chat_ids values (3, 2, 'Баскетболисты');"))
+            cursor.execute(sql.SQL("insert into chat_ids values (4, 3, 'Волейбол');"))
+            cursor.execute(sql.SQL("insert into chat_ids values (5, 1, 'Музей');"))
+            cursor.execute(sql.SQL("insert into chat_ids values (6, 2, 'Геймеры');"))          
             stmt = sql.SQL("CREATE TABLE chats (user_id BIGINT NOT NULL, "
                            "chat_id BIGINT NOT NULL, "
                            "UNIQUE (user_id, chat_id), "
                            "FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,"
                            "FOREIGN KEY (chat_id) REFERENCES chat_ids (chat_id) ON DELETE CASCADE"
                            ");")
-            cursor.execute(stmt)
+            cursor.execute(stmt) 
+            #insert into chats
+            cursor.execute(sql.SQL("Insert into chats values (1, 2);"))
+            cursor.execute(sql.SQL("Insert into chats values (2, 2);"))
+            cursor.execute(sql.SQL("Insert into chats values (3, 2);"))
+            cursor.execute(sql.SQL("Insert into chats values (1, 3);"))
+            cursor.execute(sql.SQL("Insert into chats values (2, 3);"))
+            cursor.execute(sql.SQL("Insert into chats values (3, 3);"))
+            cursor.execute(sql.SQL("Insert into chats values (1, 4);"))
+            cursor.execute(sql.SQL("Insert into chats values (2, 4);"))
+            cursor.execute(sql.SQL("Insert into chats values (3, 4);"))
+            cursor.execute(sql.SQL("Insert into chats values (1, 5);"))
+            cursor.execute(sql.SQL("Insert into chats values (2, 5);"))
+            cursor.execute(sql.SQL("Insert into chats values (3, 5);"))
+            cursor.execute(sql.SQL("Insert into chats values (1, 6);"))
+            cursor.execute(sql.SQL("Insert into chats values (2, 6);"))
+            cursor.execute(sql.SQL("Insert into chats values (3, 6);")) 
             # Single message
             cursor.execute(sql.SQL("CREATE TABLE chat_messages ("
                                    "chat_id BIGINT NOT NULL, "
@@ -94,7 +116,25 @@ def main():
                                    "time BIGINT NOT NULL,"
                                    "FOREIGN KEY (chat_id) REFERENCES chat_ids (chat_id) ON DELETE CASCADE,"
                                    "FOREIGN KEY (from_user_id) REFERENCES users (user_id) ON DELETE CASCADE"
-                                   ");"))
+                                   ");")) 
+            #insert into chat_messages						   		 
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 1, 'Давайте сегодня поиграем в футбол?', True, 1593582860);"))								   
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 1, 'Я предлагаю, как обычно в 18:00', True, 1593583160);"))
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 2, 'Я буду, но опоздаю', True, 1593583560);"))							
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 3, 'Я тоже буду', True, 1593583960);"))								   
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 1, 'Возьмите мяч', True, 1593584160);"))							   
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 3, 'Хорошо', True, 1593585160);"))								   
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 3, 'А ты возьми воды', True, 1593585160);"))								   
+            cursor.execute(sql.SQL("Insert into chat_messages values ( 2, 1,'Ок', True, 1593585360);"))								   
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 2, 'Опоздаю на 30 минут', True, 1593586760);"))								   
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 2, 'Извините', True, 1593586810);"))								   
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 1, 'Не страшно. Я приведу 2 друзей с работы', True, 1593586830);"))								  
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 1, 'Покажем им это приложение, чтобы они тоже в чате были', True, 1593586840);"))								   
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 2, 'Ок', True, 1593586810);"))								   
+            cursor.execute(sql.SQL("Insert into chat_messages values (2, 2, 'До встречи', True, 1593586810);"))
+            cursor.execute(sql.SQL("Insert into chat_messages values (3, 3, 'Сегодня в баскетбол?', True, 1593582860);"))
+            cursor.execute(sql.SQL("Insert into chat_messages values (3, 3, 'Кто в деле?', True, 1593582890);"))
+            cursor.execute(sql.SQL("Insert into chat_messages values (3, 1, 'я', True, 1593582890);"))
             # Actions
             cursor.execute(sql.SQL("CREATE TABLE actions ("
                                    "action_id BIGINT NOT NULL, "
@@ -110,6 +150,8 @@ def main():
                                    "FOREIGN KEY (owner) REFERENCES users (user_id) ON DELETE CASCADE, "
                                    "FOREIGN KEY (chat_id) REFERENCES chat_ids (chat_id) ON DELETE CASCADE"
                                    ");"))
+            #insert into actions									  
+            cursor.execute(sql.SQL("insert into actions values (36, 'Футбол на Ломоносовской', 59.88005312, 30.43818982, 2, 'Играем в футбол 5 на 5 в 19:00', 2, 1593410060, 1593619200);"))
             cursor.execute(sql.SQL("CREATE TABLE action_members ("
                                    "user_id BIGINT NOT NULL, "
                                    "action_id BIGINT NOT NULL, "
@@ -117,6 +159,10 @@ def main():
                                    "FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE, "
                                    "FOREIGN KEY (action_id) REFERENCES actions (action_id) ON DELETE CASCADE"
                                    ");"))
+            #insert into action_members		
+            cursor.execute(sql.SQL("Insert into action_members values (1, 36);"))							 					   
+            cursor.execute(sql.SQL("Insert into action_members values (2, 36);"))								 						   
+            cursor.execute(sql.SQL("Insert into action_members values (3, 36);"))
             # Crime detector
             cursor.execute(sql.SQL("CREATE TABLE geolocations ("
                                    "user_id BIGINT NOT NULL,"
